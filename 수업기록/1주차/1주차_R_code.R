3fdb8cd12495039841f7cee138e408c09d725e39
yoohyunsooo <amuuuu0521@naver.com>
2024-09-09 16:19:15 +0900

    1주차 R 코드 파일 업로드

---
+++ 수업기록/1주차/1주차_R_code.R
@@ -0,0 +1,79 @@
+# *<- / *= / <<- / -> / ->>
+a <- 1
+b <- 3
+a+b
+print(a+b) #정석
+print(213465) #숫자
+print("hello") #문자열
+
+a <- 3
+b = 4
+c = 7
+d = a+b
+
+print(a)
+print(d)
+
+print("Hello")
+
+#^ or ** 승수
+# %% 나머지 값
+# %/% 몫
+
+10%/%3
+10%%3
+
+#논리연산자, 비교연산자
+5 < 5
+5 <= 5
+5 > 5
+5 >= 5
+y <- 5 == 5
+x <- 5 != 5
+
+!TRUE
+TRUE|TRUE
+TRUE&TRUE
+isTRUE(x)
+
+
+1+2+3+4+5+6+7+8+9+10
+(1+2+3+4+5+6+7+8+9+10)/10
+1+2+3+4+5
+(1+2+3+4+5)/5
+(1-3)^2 + (2-3)^2 + (3-3)^2 + (4-3)^2 + (5-3)^2
+10/4
+13%/%4
+13%%4
+
+3>4
+3>-4
+3<4
+3<=4
+3==4
+3!=4
+!(3==4)
+
+(5<=6)&(8<=7)
+
+integer_var <- 5
+numeric_var <- 3.14
+complex_var <- 2 + 3i
+character_var <- "Hello, world!"
+logical_var <- TRUE
+another_logical_var <- FALSE
+NULL_var <- NULL
+negative_inf <- -Inf
+positive_inf <- Inf
+nan <- 0/0
+
+mode(integer_var)
+mode(numeric_var)
+mode(complex_var)
+mode(character_var)
+mode(logical_var)
+mode(another_logical_var)
+mode(NULL_var)
+mode(negative_inf) 
+mode(positive_inf)
+mode(nan)
